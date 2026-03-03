a9ef4fb81d9b53209abc629bf072ac83ea6053c1
Diann Sade <diannsad@er-d7.yks.21-school.ru>
2026-03-03 11:36:11 +0900

    fix

--- src/shared/images.ts
+++ src/shared/images.ts
@@ -1,4 +1,1 @@
-export const images = import.meta.glob(
-  "/public/projects/**/*.{png,jpg,jpeg,webp,avif}",
-  { eager: true }
-) as Record<string, { default: string }>;
+export { images } from "./images.generated";
